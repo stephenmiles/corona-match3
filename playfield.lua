bab72540a99b26ecbbfebd8c48e22c12acf30405
Steve Miles <smiles@groundbreak.com>
2014-07-10 16:49:56 -0400

    initial commit

---
+++ playfield.lua
@@ -0,0 +1,104 @@
+local composer = require( "composer" )
+local scene = composer.newScene()
+
+---------------------------------------------------------------------------------
+-- All code outside of the listener functions will only be executed ONCE
+-- unless "composer.removeScene()" is called.
+---------------------------------------------------------------------------------
+
+local physics = require "physics"
+physics.start()
+physics.setGravity( 0, 20 )
+local mydata = require( "mydata" )
+
+---------------------------------------------------------------------------------
+
+-- "scene:create()"
+function scene:create( event )
+
+   local sceneGroup = self.view
+
+   local options =
+   {
+      frames =
+      {
+        --frame 1
+        {
+            x = 0,
+            y = 0,
+            width = 75,
+            height = 75
+        },
+        --frame 2
+        {    
+            x = 75,
+            y = 0,
+            width = 75,
+            height = 75
+        }
+      }
+   }
+
+   local sheet = graphics.newImageSheet( "gems.png", options )
+
+   for i=75,75*10,75 do
+   	for e=75,75*10,75 do
+     local frame1 = display.newImage( sheet, 1, i, e )
+     physics.addBody(frame1, { density=1.0, friction=0, bounce=0 })
+     frame1.isFixedRotation = true
+    end
+   end
+   
+   local frame2 = display.newImage( sheet, 1, 75, 900)
+   physics.addBody(frame2)
+   frame2.bodyType = 'static'
+
+end
+
+-- "scene:show()"
+function scene:show( event )
+
+   local sceneGroup = self.view
+   local phase = event.phase
+
+end
+
+-- "scene:hide()"
+function scene:hide( event )
+
+   local sceneGroup = self.view
+   local phase = event.phase
+
+end
+
+-- "scene:destroy()"
+function scene:destroy( event )
+
+   local sceneGroup = self.view
+
+   -- Called prior to the removal of scene's view ("sceneGroup").
+   -- Insert code here to clean up the scene.
+   -- Example: remove display objects, save state, etc.
+end
+
+---------------------------------------------------------------------------------
+
+-- Listener setup
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+
+---------------------------------------------------------------------------------
+
+return scene
+
+
+
+
+
+
+
+
+
+
